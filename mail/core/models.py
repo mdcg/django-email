a8e2ec7c987c877ef64ff6803f0fbb9c1a54161f
Mauro de Carvalho <mauro.dcg@gmail.com>
2019-01-10 00:07:29 -0300

    typo

--- mail/core/models.py
+++ mail/core/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 
 class ConfirmationToken(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="confirmation_token")
     confirmation_key = models.CharField(max_length=100)
     is_confirmed = models.BooleanField(default=False)
